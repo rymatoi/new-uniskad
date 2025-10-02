7eae888e010247dedf1d4dd8b6a8bfaf8614194a
rymatoi <59874097+rymatoi@users.noreply.github.com>
2025-10-02 15:36:52 +0300

    Fix epure updates and enforce solid legend curves

--- app/plugins/project/visualization/views/plot_views/epure_view.py
+++ app/plugins/project/visualization/views/plot_views/epure_view.py
@@ -1,6 +1,7 @@
 from app.basic_funcs import timing_decorator
 from app.plugins.project.services.data_processors.epure_dp import EpureProcessor
 from app.plugins.project.visualization.views.plot_views.base_plot_view import BasePlotView
+from app.plugins.project.visualization.views.plot_views.mixins.plot_data import PlotDataMixin
 from app.plugins.project.visualization.widgets.epure import EpureItem
 
 
@@ -8,19 +9,95 @@ class EpureView(BasePlotView):
     DATA_PROCESSOR = EpureProcessor
 
     def __init__(self, item, main_window, parent=None):
+        self._epure_items = {}
         super().__init__(item, main_window, parent)
 
-    def add_curve(self, scatter_data, plot_data, **style):
-        epure = EpureItem(scatter_data, plot_data, style=style)
-        self.addItem(epure)
-        self.curve_items.append(epure)
-        self.selected_points[epure] = set()
+    def clear(self):
+        super().clear()
         if self.legend is not None:
-            self.legend.addItem(epure.legend_proxy, epure.name())
+            self.legend.clear()
+        self._epure_items.clear()
+        self.curve_items.clear()
+        self.selected_points.clear()
+
+    def _remove_epure(self, test_id):
+        epure = self._epure_items.pop(test_id, None)
+        if epure is None:
+            return
+
+        if self.legend is not None and self.legend.items:
+            for index, (sample, label) in enumerate(list(self.legend.items)):
+                if getattr(sample, 'item', None) == epure.legend_proxy:
+                    self.legend.items.pop(index)
+                    sample.setParentItem(None)
+                    label.setParentItem(None)
+                    break
+
+        if epure in self.curve_items:
+            self.curve_items.remove(epure)
+        self.selected_points.pop(epure, None)
+        self.removeItem(epure)
+
+    def _update_legend_label(self, epure: EpureItem) -> None:
+        if self.legend is None or not self.legend.items:
+            return
+
+        for sample, label in self.legend.items:
+            if getattr(sample, 'item', None) == epure.legend_proxy:
+                label.setText(epure.name())
+                break
+
+    def _upsert_epure(self, test_id, scatter_data, plot_data, style):
+        epure = self._epure_items.get(test_id)
+        if epure is None:
+            epure = EpureItem(scatter_data, plot_data, style=style)
+            self._epure_items[test_id] = epure
+            self.addItem(epure)
+            self.curve_items.append(epure)
+            self.selected_points[epure] = set()
+            if self.legend is not None:
+                self.legend.addItem(epure.legend_proxy, epure.name())
+        else:
+            was_visible = epure.isVisible()
+            proxy_visible = epure.legend_proxy.isVisible()
+            epure.update_data(scatter_data, plot_data, style=style)
+            epure.setVisible(was_visible)
+            epure.legend_proxy.setVisible(proxy_visible)
+            self._update_legend_label(epure)
         return epure
+
+    def add_curve(self, scatter_data, plot_data, **style):
+        test_id = style.pop('test_id', None)
+        if test_id is None:
+            # Для совместимости с миксинами, которые могут ожидать стандартное поведение
+            return PlotDataMixin.add_curve(self, scatter_data, plot_data, **style)
+        return self._upsert_epure(test_id, scatter_data, plot_data, style)
+
+    def _sync_epures(self, entries, remove_missing=True):
+        seen_ids = set()
+        for test_id, scatter_values, curve_values, style in entries:
+            seen_ids.add(test_id)
+            self._upsert_epure(test_id, scatter_values, curve_values, style)
+
+        if remove_missing:
+            stale_ids = set(self._epure_items.keys()) - seen_ids
+            for test_id in stale_ids:
+                self._remove_epure(test_id)
 
     @timing_decorator
     def prepare_curves(self):
-        self.clear()
-        for scatter_values, curve_values, style in self.data_processor.get_epure_curves():
-            self.add_curve(scatter_values, curve_values, **style)
+        epure_entries = list(self.data_processor.get_epure_curves())
+        self._sync_epures(epure_entries, remove_missing=True)
+
+    def _update_curves(self, test_ids):
+        if test_ids is None:
+            self.prepare_curves()
+            return
+
+        requested_ids = set(test_ids)
+        epure_entries = [
+            entry for entry in self.data_processor.get_epure_curves()
+            if entry[0] in requested_ids
+        ]
+        if epure_entries:
+            self._sync_epures(epure_entries, remove_missing=False)
--- app/plugins/project/visualization/widgets/epure.py
+++ app/plugins/project/visualization/widgets/epure.py
@@ -1,4 +1,5 @@
 import numpy as np
+from PySide2.QtCore import Qt
 
 from app.plugins.project.core.constants import GraphConstants
 from app.plugins.project.visualization.widgets.legend_proxy import LegendProxyPlotDataItem
@@ -9,76 +10,25 @@ class EpureItem(pg.ItemGroup):
     def __init__(self, all_scatter_data, all_plot_data, **kwargs):
         super().__init__()
 
-        self._style_config = kwargs.get('style', GraphConstants.DEFAULT_STYLE.copy()).copy()
-        if 'symbol_size' not in self._style_config:
-            self._style_config['symbol_size'] = GraphConstants.DEFAULT_STYLE['symbol_size']
-        if 'color' not in self._style_config:
-            self._style_config['color'] = GraphConstants.DEFAULT_STYLE['color']
-        if 'fill_color' not in self._style_config:
-            self._style_config['fill_color'] = self._style_config['color']
-        if 'width' not in self._style_config:
-            self._style_config['width'] = GraphConstants.DEFAULT_STYLE['width']
-        if 'symbol' not in self._style_config:
-            self._style_config['symbol'] = GraphConstants.DEFAULT_STYLE['symbol']
-        if 'symbol_color' not in self._style_config:
-            self._style_config['symbol_color'] = self._style_config['color']
+        self._style_config = self._normalize_style(kwargs.get('style'))
         self._name = self._style_config.get('name', 'Epure')
 
         # Прокси-элемент для легенды (невидимый на графике)
         self.legend_proxy = self.init_legend_proxy()
 
-        scatter_pairs = [
-            (np.asarray(x, dtype=float), np.asarray(y, dtype=float))
-            for x, y in all_scatter_data
-        ]
+        scatter_x, scatter_y = self._combine_scatter_points(all_scatter_data)
+        curve_x, curve_y = self._combine_curve_segments(all_plot_data)
 
-        if scatter_pairs:
-            scatter_x = np.concatenate([pair[0] for pair in scatter_pairs])
-            scatter_y = np.concatenate([pair[1] for pair in scatter_pairs])
-        else:
-            scatter_x = np.empty(0, dtype=float)
-            scatter_y = np.empty(0, dtype=float)
-
-        curve_pairs = [
-            (np.asarray(x, dtype=float), np.asarray(y, dtype=float))
-            for x, y in all_plot_data
-        ]
-
-        if curve_pairs:
-            total_points = sum(pair[0].size for pair in curve_pairs)
-            gap_count = max(len(curve_pairs) - 1, 0)
-
-            x_arr = np.empty(total_points + gap_count, dtype=float)
-            y_arr = np.empty(total_points + gap_count, dtype=float)
-
-            position = 0
-            for index, (x_data, y_data) in enumerate(curve_pairs):
-                length = x_data.size
-                if length:
-                    x_arr[position:position + length] = x_data
-                    y_arr[position:position + length] = y_data
-                    position += length
-
-                if index < len(curve_pairs) - 1:
-                    x_arr[position] = np.nan
-                    y_arr[position] = np.nan
-                    position += 1
-
-            if position < x_arr.size:
-                x_arr = x_arr[:position]
-                y_arr = y_arr[:position]
-        else:
-            x_arr = np.empty(0, dtype=float)
-            y_arr = np.empty(0, dtype=float)
-
-        # Создаем один scatter для всех исходных точек
+        # Создаем scatter и curve элементы
         self.scatter = self._create_scatter((scatter_x, scatter_y))
-        # Создаем одну кривую для всех интерполированных сегментов
-        self.curve = self._create_curve((x_arr, y_arr))
+        self.curve = self._create_curve((curve_x, curve_y))
 
         # Добавляем элементы в обратном порядке, чтобы точки были поверх линий
         self.addItem(self.curve)
         self.addItem(self.scatter)
+
+        self._update_curve_pen()
+        self._update_legend_proxy_style()
 
         # Синхронизация видимости
         self.legend_proxy.visibilityChanged.connect(self._on_proxy_visibility_changed)
@@ -87,13 +37,49 @@ class EpureItem(pg.ItemGroup):
     def style_config(self):
         return self._style_config
 
+    @staticmethod
+    def _ensure_opaque_color(color_value):
+        color = pg.mkColor(color_value) if color_value is not None else None
+        if color is None:
+            color = pg.mkColor(GraphConstants.DEFAULT_STYLE['color'])
+        color.setAlpha(255)
+        return color
+
+    def _normalize_style(self, style):
+        base = GraphConstants.DEFAULT_STYLE.copy()
+        if style:
+            base.update(style)
+
+        def _int_value(value, default):
+            try:
+                return int(value)
+            except (TypeError, ValueError):
+                return default
+
+        normalized = {
+            'color': self._ensure_opaque_color(base.get('color')),
+            'width': _int_value(base.get('width', GraphConstants.DEFAULT_STYLE['width']),
+                               GraphConstants.DEFAULT_STYLE['width']),
+            'symbol': base.get('symbol', GraphConstants.DEFAULT_STYLE['symbol']) or GraphConstants.DEFAULT_STYLE['symbol'],
+            'symbol_size': _int_value(base.get('symbol_size', GraphConstants.DEFAULT_STYLE['symbol_size']),
+                                      GraphConstants.DEFAULT_STYLE['symbol_size']),
+            'symbol_color': self._ensure_opaque_color(base.get('symbol_color', base.get('color'))),
+            'fill_color': self._ensure_opaque_color(base.get('fill_color', base.get('color'))),
+            'name': base.get('name', 'Epure')
+        }
+
+        # Тип линии всегда сплошной и непрозрачный для читаемости
+        normalized['line_style'] = Qt.SolidLine
+
+        return normalized
+
     def init_legend_proxy(self):
         legend_proxy = LegendProxyPlotDataItem([], [], name=self._name, style=self._style_config)
         legend_proxy.setVisible(True)
         legend_proxy.opts.update({
             'size': self._style_config.get('symbol_size', GraphConstants.DEFAULT_STYLE['symbol_size']),
-            'pen': pg.mkPen(color=self._style_config['color'],
-                            style=GraphConstants.resolve_pen_style(self._style_config.get('line_style'))),
+            'pen': pg.mkPen(color=self._style_config['color'], style=Qt.SolidLine,
+                            width=self._style_config.get('width', GraphConstants.DEFAULT_STYLE['width'])),
             'brush': pg.mkBrush(self._style_config['fill_color']),
             'symbolPen': pg.mkPen(self._style_config.get('symbol_color', self._style_config['color']))
         })
@@ -101,16 +87,18 @@ class EpureItem(pg.ItemGroup):
 
     def _create_curve(self, plot_data):
         x, y = plot_data
-        return pg.PlotCurveItem(
+        curve = pg.PlotCurveItem(
             x=x,
             y=y,
             connect='finite',
             pen=pg.mkPen(
                 color=self._style_config['color'],
                 width=self._style_config.get('width', 2),
-                style=GraphConstants.resolve_pen_style(self._style_config.get('line_style'))
+                style=Qt.SolidLine
             )
         )
+        curve.setShadowPen(None)
+        return curve
 
     def _create_scatter(self, scatter_data):
         x, y = scatter_data
@@ -122,6 +110,90 @@ class EpureItem(pg.ItemGroup):
             brush=pg.mkBrush(self._style_config['fill_color']),
             pen=pg.mkPen(color=self._style_config.get('symbol_color', self._style_config['color']))
         )
+
+    def _combine_scatter_points(self, scatter_sequences):
+        if not scatter_sequences:
+            return np.empty(0, dtype=float), np.empty(0, dtype=float)
+
+        xs = []
+        ys = []
+        for x_values, y_values in scatter_sequences:
+            x_arr = np.asarray(x_values, dtype=float)
+            y_arr = np.asarray(y_values, dtype=float)
+            if x_arr.size == 0 or y_arr.size == 0:
+                continue
+            xs.append(x_arr)
+            ys.append(y_arr)
+
+        if not xs:
+            return np.empty(0, dtype=float), np.empty(0, dtype=float)
+
+        return np.concatenate(xs), np.concatenate(ys)
+
+    def _combine_curve_segments(self, curve_sequences):
+        if not curve_sequences:
+            return np.empty(0, dtype=float), np.empty(0, dtype=float)
+
+        segments_x = []
+        segments_y = []
+        for index, (x_values, y_values) in enumerate(curve_sequences):
+            x_arr = np.asarray(x_values, dtype=float)
+            y_arr = np.asarray(y_values, dtype=float)
+            if x_arr.size == 0 or y_arr.size == 0:
+                continue
+
+            if segments_x:
+                gap = np.full(3, np.nan, dtype=float)
+                segments_x.append(gap)
+                segments_y.append(gap)
+
+            segments_x.append(x_arr)
+            segments_y.append(y_arr)
+
+        if not segments_x:
+            return np.empty(0, dtype=float), np.empty(0, dtype=float)
+
+        return np.concatenate(segments_x), np.concatenate(segments_y)
+
+    def _update_curve_pen(self):
+        pen = pg.mkPen(
+            color=self._style_config['color'],
+            width=self._style_config.get('width', GraphConstants.DEFAULT_STYLE['width']),
+            style=Qt.SolidLine
+        )
+        self.curve.setPen(pen)
+
+    def _update_legend_proxy_style(self):
+        self.legend_proxy._style_config.update(self._style_config)
+        self.legend_proxy.apply_style()
+        self.legend_proxy.opts.update({
+            'size': self._style_config.get('symbol_size', GraphConstants.DEFAULT_STYLE['symbol_size']),
+            'pen': pg.mkPen(color=self._style_config['color'], style=Qt.SolidLine,
+                            width=self._style_config.get('width', GraphConstants.DEFAULT_STYLE['width'])),
+            'brush': pg.mkBrush(self._style_config['fill_color']),
+            'symbolPen': pg.mkPen(self._style_config.get('symbol_color', self._style_config['color']))
+        })
+        self.legend_proxy.opts['name'] = self._name
+
+    def update_data(self, all_scatter_data, all_plot_data, style=None):
+        if style:
+            self._style_config = self._normalize_style(style)
+            self._name = self._style_config.get('name', self._name)
+
+        scatter_x, scatter_y = self._combine_scatter_points(all_scatter_data)
+        curve_x, curve_y = self._combine_curve_segments(all_plot_data)
+
+        self.scatter.setData(
+            x=scatter_x,
+            y=scatter_y,
+            symbol=self._style_config['symbol'],
+            size=self._style_config.get('symbol_size', GraphConstants.DEFAULT_STYLE['symbol_size']),
+            brush=pg.mkBrush(self._style_config['fill_color']),
+            pen=pg.mkPen(self._style_config.get('symbol_color', self._style_config['color']))
+        )
+        self.curve.setData(x=curve_x, y=curve_y, connect='finite')
+        self._update_curve_pen()
+        self._update_legend_proxy_style()
 
     @property
     def opts(self):
